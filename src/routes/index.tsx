b3399dd776e4f3e60f53b96c1a893b506aa1c9ee
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-08-05 06:19:18 -0300

    Now yes Finishing code, routes folder bringing back the default export

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 
-export const Routes = () => (
+const Routes = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
   </Switch>
 );
+
+export default Routes;
